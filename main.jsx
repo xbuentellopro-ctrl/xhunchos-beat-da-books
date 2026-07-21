ec8f33a33bb78ec952bbbba39badaf50f1e05099
xbuentellopro-ctrl <xbuentello.pro@gmail.com>
2026-07-21 09:23:40 -0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import EdgeBoard from './EdgeBoard.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <EdgeBoard />
-  </React.StrictMode>,
-)
